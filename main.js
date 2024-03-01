a2d0d266dcb4a0e6629766f9d33a477fe3331114
Sam <samejones2018@gmail.com>
2024-03-01 10:05:19 -0800

    added animation to the visual

--- main.js
+++ main.js
@@ -3,18 +3,18 @@ carCanvas.width=200;
 const networkCanvas=document.getElementById("networkCanvas");
 networkCanvas.width=300;
 
-
 const carCtx = carCanvas.getContext("2d");
 const networkCtx = networkCanvas.getContext("2d");
+
 const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
-const car = new Car(road.getLaneCenter(1),100,30,50,"AI",3);
-const traffic = [
-    new Car(road.getLaneCenter(1), -100,30,50,"DUMMY",2)
+const car=new Car(road.getLaneCenter(1),100,30,50,"AI");
+const traffic=[
+    new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",2)
 ];
 
 animate();
 
-function animate(){
+function animate(time){
     for(let i=0;i<traffic.length;i++){
         traffic[i].update(road.borders,[]);
     }
@@ -25,14 +25,16 @@ function animate(){
 
     carCtx.save();
     carCtx.translate(0,-car.y+carCanvas.height*0.7);
+
     road.draw(carCtx);
     for(let i=0;i<traffic.length;i++){
         traffic[i].draw(carCtx,"red");
     }
-    car.draw(carCtx, "blue");
+    car.draw(carCtx,"blue");
 
     carCtx.restore();
 
+    networkCtx.lineDashOffset=-time/50;
     Visualizer.drawNetwork(networkCtx,car.brain);
     requestAnimationFrame(animate);
 }
